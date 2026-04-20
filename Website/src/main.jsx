42c8bcb93098a717aafe939024454b184d6136dd
Soah Fernandes <soah.fernandes2005@gmail.com>
2026-04-20 20:48:04 +0530

    Added vercel speed insights and other

--- Website/src/main.jsx
+++ Website/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import DeferredAnalytics from './components/DeferredAnalytics.jsx'
+import DeferredSpeedInsights from './components/DeferredSpeedInsights.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <DeferredAnalytics />
+    <DeferredSpeedInsights />
   </StrictMode>,
 )
